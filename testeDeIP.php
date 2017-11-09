2f3776c0cdc67f6e33eee9f20ec0181490f24c51
Igor Duarte <igorfun@hotmail.com>
2017-11-09 16:24:31 -0200

    teste2

--- testeDeIP.php
+++ testeDeIP.php
@@ -3,3 +3,9 @@ echo $_SERVER["SERVER_ADDR"];
 echo "</br>";
 echo $_SERVER["REMOTE_ADDR"];
 echo "</br>";
+$host= gethostname();
+$ip = gethostbyname($host)
+echo $host;
+echo "</br>";
+echo $ip;
+echo "</br>";
